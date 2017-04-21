1775172c91fe63519122b4de30aa57e2e5cf25d7
Antonio Furtado <antonio@ajency.in>
2017-04-21 16:51:32 +0530

    Structure for views

---
+++ events/views/table-view-item-bootstrap.php
@@ -0,0 +1,16 @@
+<div class="aj-table__info row">
+    <div data-display="Event Title" class="aj-table__name col-md-6 col-sm-6">
+						<span class="aj__price aj-table__price pull-right">
+							<small>&#8377;</small> <strong>35</strong>
+						</span>
+        <h4 class="aj-title aj-table__title"><a href="#"><?php echo $event['title']; ?></a></h4>
+        <p class="aj-table__desc">Seamlessly actualize parallel technologies and multidisciplinary technologies...</p>
+    </div>
+    <div data-display="Location" class="aj__address aj-table__address col-md-3 col-sm-3"><a href="#">Kala Academy, Panjim, Goa</a></div>
+    <div data-display="Date" class="aj-table__date col-md-3 col-sm-3">
+						<span class="aj-table__date-wrap">
+							<strong>23 Feb - 15 Mar 2017</strong>
+							05:30PM - 08:30PM
+						</span>
+    </div>
+</div>
---
+++ public/custom.js
@@ -0,0 +1,81 @@
+(function( $ ) {
+	'use strict';
+
+	/**
+	 * All of the code for your admin-facing JavaScript source
+	 * should reside in this file.
+	 *
+	 * Note: It has been assumed you will write jQuery code here, so the
+	 * $ function reference has been prepared for usage within the scope
+	 * of this function.
+	 *
+	 * This enables you to define handlers, for when the DOM is ready:
+	 *
+	 * $(function() {
+	 *
+	 * });
+	 *
+	 * When the window is loaded:
+	 *
+	 * $( window ).load(function() {
+	 *
+	 * });
+	 *
+	 * ...and/or other possibilities.
+	 *
+	 * Ideally, it is not considered best practise to attach more than a
+	 * single DOM-ready or window-load handler for a particular page.
+	 * Although scripts in the WordPress core, Plugins and Themes may be
+	 * practising this, we should strive to set a better example in our own work.
+	 */
+
+	jQuery(document).ready(function() {
+
+		$('#load-more').click(function(e) {
+			e.preventDefault();
+//Send the AJAX call to the server
+			$.ajax({
+				//The URL to process the request
+				'url' : 'http://wp.dev/wp-content/plugins/eventcodes/events/example-json.php',
+				//The type of request, also known as the "method" in HTML forms
+				//Can be 'GET' or 'POST'
+				'type' : 'GET',
+				//Any post-data/get-data parameters
+				//This is optional
+				'data' : {
+					'paramater1' : 'value',
+					'parameter2' : 'another value'
+				},
+				//The response from the server
+				'success' : function(data) {
+
+					$.ajax({
+						//The URL to process the request
+						'url' : 'http://wp.dev/wp-content/plugins/eventcodes/events/views/table-view-item-bootstrap.php',
+						//The type of request, also known as the "method" in HTML forms
+						//Can be 'GET' or 'POST'
+						'type' : 'GET',
+						//Any post-data/get-data parameters
+						//This is optional
+						'data' : {
+							'paramater1' : 'value',
+							'parameter2' : 'another value'
+						},
+						//The response from the server
+						'success' : function(markup) {
+							for(var i = 0; i < data.length; i ++) {
+								var markup1 = $(markup);
+								var a = markup1.find('.aj-title').find('a');
+								var event = data[i];
+								$(a[0]).text(event.title);
+								$('.aj-table').append(markup1);
+							}
+						}
+					});
+				}
+			});
+		});
+	});
+
+
+})( jQuery );
