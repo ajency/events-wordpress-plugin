5c7d8d41dd404f541815ef0c9fa56c5c4349defd
Antonio Furtado <antonio@ajency.in>
2017-04-24 11:50:06 +0530

    Provision for Dummy Data, Changed structure

---
+++ events/views/bootstrap/tabular-view.php
@@ -0,0 +1,34 @@
+<!--
+    T A B L E
+    styles: basic / shadow
+    ===================================
+    Add/replace the beolow classes to the div with class "aj-table"
+    ===================================
+    class for view: aj-table--basic / aj-table--shadow
+    class for alt row grey: aj-table--alternate
+    class for price: aj--hasprice
+    class for time: aj--hastime
+    class for desc: aj--hasdesc
+-->
+<div class="aj">
+    <h3 class="aj__title">Upcoming Events</h3>
+        <div class="aj-table aj-table--<?php echo $atts['style']; ?> aj--hasprice aj--hastime aj--hasdesc">
+            <div class="aj-table__info aj-table--header row">
+                <div class="aj-table__name col-md-6 col-sm-6">Event Title</div>
+                <div class="aj__address aj-table__address col-md-3 col-sm-3">Location</div>
+                <div class="aj-table__date col-md-3 col-sm-3">Date</div>
+            </div>
+            <?php foreach($event_data as $event) : ?>
+                <?php include(dirname( __FILE__ )  . '/tabular-view-item.php' ); ?>
+            <?php endforeach; ?>
+        </div>
+        <div class="text-center aj-table__navi">
+            <a id="load-more" href="" class="aj__link aj--loadmore">Load More</a>
+            <input type="hidden" id="style" value="<?php echo $config['style']; ?>" />
+            <a href="#" class="aj__button aj--viewall btn btn-primary pull-right">View All</a>
+        </div>
+</div>
+<!--
+    / E N D
+    T A B L E
+-->
---
+++ events/views/normal/tabular-view-item.php
@@ -0,0 +1,20 @@
+<div class="aj-table__info">
+    <div data-display="Event Title" class="aj-table__name">
+						<span class="aj__price aj-table__price aj-list__price">
+							<small class="aj__data-currency">&#8377;</small> <strong class="aj__data-price">35</strong>
+						</span>
+        <h4 class="aj-table__title aj__data-title"><a href="#">Serenity - A watercolor art exhibition</a></h4>
+        <p class="aj-table__desc aj__data-desc">Seamlessly actualize parallel technologies and multidisciplinary technologies...</p>
+    </div>
+    <div data-display="Location" class="aj__address aj-table__address aj__data-address"><a href="#">Kala Academy, Panjim, Goa</a></div>
+    <div data-display="Date" class="aj-table__date">
+						<span class="aj-table__date-wrap">
+							<strong>
+                                <span class="aj__data-daystart">16</span> <span class="aj__data-daystart-month">May</span>
+                                -
+                                <span class="aj__data-dayend">18</span> <span class="aj__data-dayend-month">May</span>
+                            </strong>
+							<span class="aj__data-time">09:30AM - 02:30PM</span>
+						</span>
+    </div>
+</div>
