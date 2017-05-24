67ef6d82081b3d1abaf857111475f5b114e31b7a
Antonio Furtado <antonio@ajency.in>
2017-05-24 17:13:21 +0530

    Normal Grid view added

---
+++ events/views/bootstrap/grid-view-item.php
@@ -0,0 +1,75 @@
+<div class="aj-list__single">
+    <div class="aj-list__inner">
+        <div class="aj-list__top">
+
+            <?php
+            if(!$event['art_work']){
+                $event['art_work'] = plugin_dir_url( __DIR__ ).'img/dummyimg.png';
+            }
+            ?>
+
+            <a href="<?php echo $event['title_link']; ?>" class="aj-list__image aj__data-image" style="background-image: url(<?php echo $event['art_work']; ?>);">
+                <img src="<?php echo $event['art_work']; ?>" alt="">
+            </a>
+        </div>
+        <div class="aj-list__content">
+
+
+            <?php if($event['price']) : ?>
+                <div class="aj__price aj-list__price pull-right">
+                    <?php if(!$event['currency_position'] || $event['currency_position'] == 'prefix') : ?>
+
+                        <small class="aj__data-currency"><?php echo $event['currency']; ?></small>
+                    <?php endif; ?>
+
+                    <strong class="aj__data-price"><?php echo $event['price']; ?></strong>
+                    <?php if($event['currency_position'] == 'suffix') : ?>
+                        <small class="aj__data-currency"><?php echo $event['currency']; ?></small>
+                    <?php endif; ?>
+
+
+                </div>
+            <?php endif; ?>
+            <div class="aj-list__date">
+
+								<span class="aj-list__datein">
+									<span class="aj-list__start">
+										<span class="aj-list__day aj__data-daystart"><?php echo $event['start_date_day']; ?></span>
+										<span class="aj__data-daystart-month"><?php echo $event['start_date_mon']; ?></span>
+									</span>
+                                    <?php if($event['end_date_day']) : ?>
+                                        <span class="aj-list__div">-</span>
+                                        <span class="aj-list__end">
+										<span class="aj-list__day aj__data-dayend"><?php echo $event['end_date_day']; ?></span>
+										<span class="aj__data-dayend-month"><?php echo $event['end_date_mon']; ?></span>
+									</span>
+                                    <?php endif; ?>
+								</span>
+								<span class="aj-list__time aj__data-time">
+									<span class="aj__data-time">
+                                        <?php if($event['start_time']) : ?>
+                                            <span class="aj__data-timestart"><?php echo $event['start_time']; ?></span>
+                                        <?php endif; ?>
+                                        <?php if($event['end_time']) : ?>
+                                            <span class="aj__data-hyphen">-</span>
+                                            <span class="aj__data-timeend"><?php echo $event['end_time']; ?></span>
+                                        <?php endif; ?>
+									</span>
+								</span>
+            </div>
+
+            <h4 class="aj-list__title aj__data-title"><a href="<?php echo $event['title_link']; ?>"><?php echo $event['title']; ?></a></h4>
+            <?php if($event['address_link']): ?>
+                <div class="aj__address aj-list__address aj__data-address">
+                    <a target="_blank" href="<?php echo $event['address_link']; ?>"><img src="<?php echo plugin_dir_url( __DIR__ ); ?>img/marker.png" alt=""><?php echo $event['address']; ?></a>
+                </div>
+            <?php else: ?>
+                <div class="aj__address aj-list__address aj__data-address">
+                    <img src="<?php echo plugin_dir_url( __DIR__ ); ?>img/marker.png" alt=""><?php echo $event['address']; ?>
+                </div>
+            <?php endif; ?>
+
+            <p class="aj-list__desc aj__data-desc"><?php echo $event['description']; ?></p>
+        </div>
+    </div>
+</div>
---
+++ events/views/normal/grid-view.php
@@ -0,0 +1,40 @@
+<!--
+			G R I D
+			styles: basic / big-date / overlay
+			===================================
+			Add/replace the beolow classes to the div with class "aj-grid"
+			===================================
+			class for layout: aj-grid--basic / aj-grid--bigdate / aj-grid--overlay
+			class for price: aj--hasprice
+			class for time: aj--hastime
+			class for desc: aj--hasdesc
+		-->
+<div class="aj">
+    <h3 class="aj__title"><?php echo $event_data->event_range_lbl; ?> Events</h3>
+    <div id="data-<?php echo $shortcode_id; ?>" class="aj-grid aj-grid--overlay <?php echo $atts['description'] ? 'aj--hasdesc':'' ?> <?php echo $atts['showtime'] ? 'aj--hastime':'' ?> aj--hasprice">
+
+        <?php foreach($event_data->events as $event) : ?>
+            <?php include(dirname( __FILE__ )  . '/grid-view-item.php' ); ?>
+        <?php endforeach; ?>
+
+    </div>
+    <div class="text-center aj__navi">
+        <?php if($atts['show-load-more'] && $atts['count'] < $event_data->count) : ?>
+            <a id="<?php echo $shortcode_id; ?>" href="" class="aj__link aj--loadmore"><?php echo sprintf(esc_html(__('Load More','event-codes'))); ?></a>
+        <?php endif; ?>
+        <?php if($atts['show-view-all']) : ?>
+            <a href="<?php echo esc_url( tribe_get_events_link() ); ?>" class="aj__button aj--viewall"><?php echo sprintf(esc_html(__('View All','event-codes'))); ?></a>
+        <?php endif; ?>
+    </div>
+    <?php if($atts['show-load-more'] && $atts['count'] < $event_data->count) : ?>
+        <script type="text/javascript">
+            /* <![CDATA[ */
+            var event_codes_sc_atts_<?php echo $shortcode_id; ?> = <?php print json_encode($atts); ?>;
+            /* ]]> */
+        </script>
+    <?php endif; ?>
+</div>
+<!--
+    / E N D
+    G R I D
+-->
