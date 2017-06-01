e1378816e72627709a223fc51b2e9b23616985e1
Antonio Furtado <antonio@ajency.in>
2017-06-01 12:44:54 +0530

    Structure changes

--- datasources/configs/class-event-codes-the-events-calendar.php
+++ datasources/configs/class-event-codes-the-events-calendar.php
@@ -7,121 +7,14 @@ class Event_Codes_The_Events_Calender extends Event_Codes_Datasource_Config {
     const GROOT = 'groot';
     const ROCKET = 'rocket';
     const MIN_VERSION_SUPPORT = 3.10;
-    const DEFAULT_VERSION_SUPPORT = 3.10;
 
     public function supported_versions_config()
     {
         //TODO in later version we might need these depending on wordpress versions
         return
-#            GROOT => ['4.4.5','4.4.4','4.4.3','4.4.2','4.4.1','4.4']
-            //We changed this array as its easier to array search by key -- less expensive
-            //The earlier would lead to MD array and array search below 5.5 is bad
             [
-                '4.5.2.1' => self::GROOT,
-
-                '4.5.0.2' => self::GROOT,
-                '4.5.0.1' => self::GROOT,
-                '4.5' => self::GROOT,
-                '4.4.5' => self::GROOT,
-                '4.4.4' => self::GROOT,
-                '4.4.3' => self::GROOT,
-                '4.4.2' => self::GROOT,
-                '4.4.1.1' => self::GROOT,
-                '4.4.1' => self::GROOT,
-                '4.4.0.1' => self::GROOT,
-                '4.4' => self::GROOT,
-                '4.3.5' => self::GROOT,
-                '4.3.4.2' => self::GROOT,
-                '4.3.4.1' => self::GROOT,
-                '4.3.4' => self::GROOT,
-                '4.3.3' => self::GROOT,
-                '4.3.2' => self::GROOT,
-                '4.3.1.1' => self::GROOT,
-                '4.3.1' => self::GROOT,
-                '4.3.0.1' => self::GROOT,
-                '4.3' => self::GROOT,
-                '4.2.7' => self::GROOT,
-                '4.2.6' => self::GROOT,
-                '4.2.5' => self::GROOT,
-                '4.2.4' => self::GROOT,
-                '4.2.3' => self::GROOT,
-                '4.2.2' => self::GROOT,
-                '4.2.1.1' => self::GROOT,
-                '4.2.1' => self::GROOT,
-                '4.2' => self::GROOT,
-                '4.1.4' => self::GROOT,
-                '4.1.3' => self::GROOT,
-                '4.1.2' => self::GROOT,
-                '4.1.1.1' => self::GROOT,
-                '4.1.1' => self::GROOT,
-                '4.1.0.1' => self::GROOT,
-                '4.1' => self::GROOT,
-                '4.0.7' => self::GROOT,
-                '4.0.6' => self::GROOT,
-                '4.0.5' => self::GROOT,
-                '4.0.4' => self::GROOT,
-                '4.0.3' => self::GROOT,
-                '4.0.2' => self::GROOT,
-                '4.0.1' => self::GROOT,
-                '4.0' => self::GROOT,
-                '3.12.6' => self::GROOT,
-                '3.12.5' => self::GROOT,
-                '3.12.4' => self::GROOT,
-                '3.12.3' => self::GROOT,
-                '3.12.2' => self::GROOT,
-                '3.12.1' => self::GROOT,
-                '3.12' => self::GROOT,
-                '3.11.2' => self::GROOT,
-                '3.11.1' => self::GROOT,
-                '3.11' => self::GROOT,
-                '3.10.1' => self::GROOT,
-                '3.10' => self::GROOT,
-                /*'3.9.3' => self::GROOT,
-                '3.9.2' => self::GROOT,
-                '3.9.1' => self::GROOT,
-                '3.9' => self::GROOT,
-                '3.8.1' => self::GROOT,
-                '3.8' => self::GROOT,
-                '3.7' => self::GROOT,
-                '3.6.1' => self::GROOT,
-                '3.6' => self::GROOT,
-                '3.5.1' => self::GROOT,
-                '3.5' => self::GROOT,
-                '3.4.1' => self::GROOT,
-                '3.4' => self::GROOT,
-                '3.3.1' => self::GROOT,
-                '3.3' => self::GROOT,
-                '3.2' => self::GROOT,
-                '3.1' => self::GROOT,
-                '3.0.3' => self::GROOT,
-                '3.0.2' => self::GROOT,
-                '3.0.1' => self::GROOT,
-                '3.0' => self::GROOT,
-                '2.0.11' => self::GROOT,
-                '2.0.10' => self::GROOT,
-                '2.0.9' => self::GROOT,
-                '2.0.8' => self::GROOT,
-                '2.0.7' => self::GROOT,
-                '2.0.6' => self::GROOT,
-                '2.0.5' => self::GROOT,
-                '2.0.4' => self::GROOT,
-                '2.0.3' => self::GROOT,
-                '2.0.2' => self::GROOT,
-                '2.0.1' => self::GROOT,
-                '2.0' => self::GROOT,
-                '1.6.5' => self::GROOT,
-                '1.6.4' => self::GROOT,
-                '1.6.3' => self::GROOT,
-                '1.6.2' => self::GROOT,
-                '1.6.1' => self::GROOT,
-                '1.6' => self::GROOT,
-                '1.5.6' => self::GROOT,
-                '1.5.5' => self::GROOT,
-                '1.5.4' => self::GROOT,
-                '1.5.3' => self::GROOT,
-                '1.5.2' => self::GROOT,
-                '1.5.1' => self::GROOT,
-                '1.5' => self::GROOT,*/
+                'default' => self::GROOT,
+                '3.9' => self::ROCKET //If we did have a different codebase for < 3.10
             ];
     }
 
@@ -135,15 +28,13 @@ class Event_Codes_The_Events_Calender extends Event_Codes_Datasource_Config {
             defined('Tribe__Events__Main::VERSION')
         ) {
             $version = Tribe__Events__Main::VERSION;
-#            $supported_versions = $this->supported_versions_config();
-/*            if(in_array($version,array_keys($supported_versions)))
-            {
-                //return the codename
-                return $supported_versions[$version];
-            }*/
-            $min_version = self::MIN_VERSION_SUPPORT;
-            if($version > $min_version) {
-                return self::GROOT;
+            $supported_versions = $this->supported_versions_config();
+
+            if($version > self::MIN_VERSION_SUPPORT) {
+                if(in_array($version,$supported_versions)){
+                    return $supported_versions[$version];
+                }
+                return $supported_versions['default'];
             }
         }
         return false;
